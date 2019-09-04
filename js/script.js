0efb264fd3932565202dcc166218a571edafaf61
sanusmd <just4deen@yahoo.com>
2019-09-04 13:23:50 +0100

    add  functionality to calculator

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 const result = document.querySelector('.cal-display');
-const clear = document.querySelector('#clear')
+const clear = document.querySelector('#clear');
+const compute = document.querySelector('.btn-equal');
+
 
 const buttons = document.querySelectorAll(".num, .op")
-console.log(buttons)
+
+// loop trough the buttons to access each button 
 Array.from(buttons).forEach((button) =>{
+
+// Add event listener 
 button.addEventListener('click', function(event) {
-result.innerHTML += button.value
-    console.log('clicked me')
+
+// display result when click on button
+result.innerHTML += button.value; 
 });
 }) ;
 
+// compute value display result div
+compute.addEventListener('click', function(){
+    result.innerHTML = eval(result.innerHTML)
+});
+
+// clear the result 
 clear.addEventListener('click', function(){
     result.innerHTML = ''
 })
