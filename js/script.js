ef2a7b1fa9b8b65ef404953047ca22818398d2f5
sanusmd <just4deen@yahoo.com>
2019-09-08 17:47:06 +0100

    Fix  equal (=) functionality

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
 const result = document.querySelector('.cal-display');
 const clear = document.querySelector('#clear');
 const compute = document.querySelector('.btn-equal');
+const decimal = document.querySelector('#decimal')
+console.log(decimal)
 
 
 const buttons = document.querySelectorAll(".num, .op")
+let input = [];
 
 // loop trough the buttons to access each button 
 Array.from(buttons).forEach((button) =>{
 
 // Add event listener 
 button.addEventListener('click', () => {
-
+const operators = ['+', '-', '/', '*']
 // display result when click on button
-result.value += button.value; 
+console.log(input, button)
+if (input[input.length-1] == '=' && !operators.includes(button.value)) {
+    result.value = button.value;
+} else {
+    result.value += button.value;
+}
+
+input.push(button.value);
 });
-}) ;
+});
+ 
 
 // compute value display in result div
 compute.addEventListener('click', () =>{
+    input.push('=');
     
     if(result.value.length == 0){
         return false
@@ -26,6 +38,9 @@ compute.addEventListener('click', () =>{
     }
     
 });
+
+
+
 
 // clear the result 
 clear.addEventListener('click', () => {
